465147dc1e53b7788038ef875d99a725d94f5d9e
Allen <72219631+Allenwins26@users.noreply.github.com>
2020-11-14 17:32:40 -0500

    Update python-snake-game.py

--- python-snake-game.py
+++ python-snake-game.py
@@ -12,3 +12,18 @@ black = (0, 0, 0)
 screen_width = 900
 screen_height = 600
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
+
+# Game Title
+pygame.display.set_caption("Allen's Snake Game")
+pygame.display.update()
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 55)
+
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x,y])
+
+
+def plot_snake(gameWindow, color, snk_list, snake_size):
+    for x,y in snk_list:
+        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
