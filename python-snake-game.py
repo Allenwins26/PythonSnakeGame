4924c637c72b7b73a6c4414afe8670dfa253b28d
Allen <72219631+Allenwins26@users.noreply.github.com>
2020-11-14 17:32:00 -0500

    Update python-snake-game.py

--- python-snake-game.py
+++ python-snake-game.py
@@ -3,3 +3,12 @@ import random
 # initializing pygame
 pygame.init()
 
+# Colors
+white = (255, 255, 255) # rgb format
+red = (255, 0, 0)
+black = (0, 0, 0)
+
+# Creating window
+screen_width = 900
+screen_height = 600
+gameWindow = pygame.display.set_mode((screen_width, screen_height))
